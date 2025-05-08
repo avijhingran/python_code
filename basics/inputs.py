0122ecf2ebfc6051d8e0e2127f0a5e065c981a27
avijhingran <49613529+avijhingran@users.noreply.github.com>
2025-05-08 17:55:58 +0530

    Create inputs.py

---
+++ basics/inputs.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+balance = float(input("Enter your account balance: "))
+
+print(f"Welcome {name}, Age: {age}, Balance: ${balance:.2f}")
